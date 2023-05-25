400128e82bb9fc3604ba19ac5e265d5644187ecd
Imran <imranmohammad098@gmail.com>
2023-05-25 23:24:18 +0530

    added query file which creates db and tables. Added the remote mysql db from inifinity free

--- config.mjs
+++ config.mjs
@@ -1,8 +1,17 @@
+// export const CONFIG = {
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: "#HiHyderabad1234",
+//     database: "hoodiehub",
+//     port: 3306,
+//     insecureAuth: true,
+// }
+
 export const CONFIG = {
-    host: "127.0.0.1",
-    user: "root",
-    password: "#HiHyderabad1234",
-    database: "hoodiehub",
+    host: "sql209.epizy.com",
+    user: "epiz_34285963",
+    password: "#l2Qn0GkgJX",
+    database: "epiz_34285963_hoodiehub",
     port: 3306,
     insecureAuth: true,
 }
