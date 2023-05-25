4191f67f51e1af48485ec881a6605daf499a01da
Imran <imranmohammad098@gmail.com>
2023-05-25 20:26:03 +0530

    optimise the code by creating seperate function for repeated code

--- server.mjs
+++ server.mjs
@@ -1,9 +1,8 @@
 import express from "express";
 import { createConnection } from "mysql";
 import cors from "cors";
-import { error } from "console";
-import { resolve } from "path";
-import { rejects } from "assert";
+
+
 const connection = createConnection({
   host: "127.0.0.1",
   user: "root",
@@ -12,6 +11,7 @@ const connection = createConnection({
   port: 3306,
   insecureAuth: true,
 });
+
 connection.connect((err) => {
   if (err) {
     console.error(err);
@@ -39,36 +39,7 @@ app.get('/products/:page/:perpage', async (req, res) => {
         }
       });
     });
-
-    const data = [];
-
-    for (const product of products) {
-      const images = await new Promise((resolve, reject) => {
-        connection.query(`SELECT imgurl FROM images WHERE prodid=${product.id}`, (err, result, fields) => {
-          if (err) {
-            console.error(`Error in image query for product ID ${product.id}`, err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-      const sizes = await new Promise((resolve, reject) => {
-        connection.query(`SELECT s,l,m,xl FROM sizetable WHERE prodid=${product.id}`, (err, result, fields) => {
-          if (err) {
-            console.error(`Error in image query for product ID ${product.id}`, err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-      product.img = JSON.parse(JSON.stringify(images));
-      product.sizes = JSON.parse(JSON.stringify(sizes));
-      data.push(product);
-    }
-
-    res.send(data);
+    addingImagesSizes(products,res);
   } catch (error) {
     console.error("Error in request", error);
     res.status(500).send("Internal Server Error");
@@ -77,7 +48,7 @@ app.get('/products/:page/:perpage', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
   try {
-    const prod = await new Promise((resolve, reject) => {
+    const products = await new Promise((resolve, reject) => {
       connection.query(`select * from products where id = ${req.params.id}`, (err, result, feilds) => {
         if (err) {
           console.log("Error in fetching image ", err);
@@ -87,36 +58,7 @@ app.get('/products/:id', async (req, res) => {
         }
       });
     });
-
-    const data = [];
-    //const products = await prod; // Await the promise to resolve
-
-    for (const p of prod) {
-      const images = await new Promise((resolve, reject) => {
-        connection.query(`SELECT imgurl FROM images WHERE prodid=${p.id}`, (err, result, fields) => {
-          if (err) {
-            console.error(`Error in image query for product ID ${p.id}`, err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-      const sizes = await new Promise((resolve, reject) => {
-        connection.query(`SELECT s,l,m,xl FROM sizetable WHERE prodid=${p.id}`, (err, result, fields) => {
-          if (err) {
-            console.error(`Error in image query for product ID ${p.id}`, err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-      p.img = JSON.parse(JSON.stringify(images));
-      p.sizes = JSON.parse(JSON.stringify(sizes));
-      data.push(p);
-    }
-    res.send(data);
+    addingImagesSizes(products,res)
   }
   catch (error) {
     console.error("Error in request", error);
@@ -135,35 +77,7 @@ app.get('/similarproducts/:inputString/:start/:items', async (req, res) => {
         }
       });
     });
-    const data = [];
-
-    for (const product of products) {
-      const images = await new Promise((resolve, reject) => {
-        connection.query(`SELECT imgurl FROM images WHERE prodid=${product.id}`, (err, result, fields) => {
-          if (err) {
-            console.error(`Error in image query for product ID ${product.id}`, err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-      const sizes = await new Promise((resolve, reject) => {
-        connection.query(`SELECT s,l,m,xl FROM sizetable WHERE prodid=${product.id}`, (err, result, fields) => {
-          if (err) {
-            console.error(`Error in image query for product ID ${product.id}`, err);
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-      product.img = JSON.parse(JSON.stringify(images));
-      product.sizes = JSON.parse(JSON.stringify(sizes));
-      data.push(product);
-    }
-
-    res.send(data);
+    addingImagesSizes(products,res);
   } catch (error) {
     console.error("Error in request", error);
     res.status(500).send("Internal Server Error");
@@ -182,27 +96,38 @@ app.get(`/seachItems/:searchText`,(req,res)=>{
       }
     })
 })
-app.get(`/pagination/products/:page/:perpage`,async (req,res)=>{
-  try{
-    const rows = await new Promise((resolve,reject)=>{
-      connection.query(`SELECT * FROM products LIMIT ${req.params.page},${req.params.perpage}`,
-        (err,result,fields)=>{
-          if(err){
-            console.log("error in qerry",err);
-            reject(err);
-          }
-          else{
-            resolve(result);
-          }
+
+const addingImagesSizes = async (products,res)=>{
+  let data = []
+  for (const product of products) {
+    const images = await new Promise((resolve, reject) => {
+      connection.query(`SELECT imgurl FROM images WHERE prodid=${product.id}`, (err, result, fields) => {
+        if (err) {
+          console.error(`Error in image query for product ID ${product.id}`, err);
+          reject(err);
+        } else {
+          resolve(result);
         }
-      )
-    })
-    res.send(rows);
+      });
+    });
+    const sizes = await new Promise((resolve, reject) => {
+      connection.query(`SELECT s,l,m,xl FROM sizetable WHERE prodid=${product.id}`, (err, result, fields) => {
+        if (err) {
+          console.error(`Error in image query for product ID ${product.id}`, err);
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    product.img = JSON.parse(JSON.stringify(images));
+    product.sizes = JSON.parse(JSON.stringify(sizes));
+    data.push(product);
   }
-  catch(err){
-    console.log("Error access the path ",err);
-  }
-})
+  console.log(data);
+  res.send(data);
+}
+
 app.listen(3000, () => {
   console.log("server running....");
 })
